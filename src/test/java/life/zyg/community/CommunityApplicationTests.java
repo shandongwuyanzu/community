023789c85f0e8da25bbb5e21f0b1147d3ff48077
zyg <465006760@qq.com>
2019-12-24 23:45:02 +0800

    zyg

---
+++ src/test/java/life/zyg/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package life.zyg.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
