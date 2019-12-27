14f5eba8e04462e5e9a354c4fa48ff9aed35ec2c
zyg <465006760@qq.com>
2019-12-27 09:34:23 +0800

    modify

--- src/main/java/life/zyg/community/Controller/AuthorizeController.java
+++ src/main/java/life/zyg/community/Controller/AuthorizeController.java
@@ -3,11 +3,18 @@ package life.zyg.community.Controller;
 import life.zyg.community.Provider.GithubProvider;
 import life.zyg.community.dto.AccessTokenDTO;
 import life.zyg.community.dto.GithubUser;
+import life.zyg.community.mapper.UserMapper;
+import life.zyg.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import reactor.netty.http.server.HttpServerRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -19,9 +26,14 @@ public class AuthorizeController {
     private String client_secret;
     @Value("${github.redirect.uri}")
     private String redirect_uri;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(client_id);
         accessTokenDTO.setClient_secret(client_secret);
@@ -29,9 +41,23 @@ public class AuthorizeController {
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.githubUser(accessToken);
-        System.out.println(user.getName()   );
-        return "index";
+        GithubUser githubUser = githubProvider.githubUser(accessToken);
+        if(githubUser!=null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登录成功，写入cookie和session
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        }else {
+            //登陆失败，重新登陆
+            return "redirect:/";
+        }
+
     }
 
 }
---
+++ src/main/java/life/zyg/community/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package life.zyg.community.mapper;
+
+import life.zyg.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (account_id,name,token,gmt_create,gmt_modified) values (#{accountId},#{name},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}
